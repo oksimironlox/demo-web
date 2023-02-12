71849da59ebd91c349a80afc5da44d67b8b0b54e
Настя <nastamezenceva9155@gmail.com>
2023-02-12 15:26:40 +0500

    createPost

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,23 +1,30 @@
 package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Date;
 
 @Service
 public class PostService {
-    public List<Post> listAllPosts(){
-        ArrayList<Post> list = new ArrayList<>();
 
-        Post post1 = new Post("1", Date.from(Instant.now()));
-        post1.setLikes(576789924);
-        Post post2 = new Post("2", Date.from(Instant.now()));
-        Post post3 = new Post("3", Date.from(Instant.now()));
-        list.add(post1); list.add(post2); list.add(post3);
-        return list;
+    List<Post> posts = new ArrayList<>() {{
+        add(new Post("1", new Date()));
+        add(new Post("2", new Date()));
+        add(new Post("3", new Date()));
+    }};
+
+
+    public List<Post> listAllPosts() {
+        return posts;
+    }
+
+    public void createPost(String text){
+        posts.add(new Post(text, new Date()));
     }
 }
