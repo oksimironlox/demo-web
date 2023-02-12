32329c5c706535488b86ff06036e1af2b7d248b6
Настя <nastamezenceva9155@gmail.com>
2023-02-12 13:07:59 +0500

    layout

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -3,13 +3,20 @@ package com.example.demoweb.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class Post {
     private String text;
     private int likes;
+    private Date creationDate;
     public Post(String text){
         this.text = text;
+    }
+    public Post(String text, Date date){
+        this.text = text;
+        creationDate = date;
     }
     public String getText(){
         return text;
@@ -17,10 +24,14 @@ public class Post {
     public int getLikes(){
         return likes;
     }
+    public Date getDate(){
+        return creationDate;
+    }
     public void setText(String text){
         this.text = text;
     }
     public void setLikes(int likes){
         this.likes = likes;
     }
+
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -3,17 +3,20 @@ package com.example.demoweb.service;
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
 
 @Service
 public class PostService {
     public List<Post> listAllPosts(){
         ArrayList<Post> list = new ArrayList<>();
-        Post post1 = new Post("1");
+
+        Post post1 = new Post("1", Date.from(Instant.now()));
         post1.setLikes(576789924);
-        Post post2 = new Post("2");
-        Post post3 = new Post("3");
+        Post post2 = new Post("2", Date.from(Instant.now()));
+        Post post3 = new Post("3", Date.from(Instant.now()));
         list.add(post1); list.add(post2); list.add(post3);
         return list;
     }
